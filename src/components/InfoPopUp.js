9818536799f04882c84da214ac69158430ebbce6
Samuel Surgeon <ssurgeon00@icloud.com>
2020-02-29 12:00:25 +1100

    Rewrite info

--- src/components/InfoPopUp.js
+++ src/components/InfoPopUp.js
@@ -14,9 +14,9 @@ export default class InfoPopUp extends React.Component {
     return (
       <section className="component-info-pop-up">
         <p className="info-heading">What is a sorting algorithm?</p>
-        <p>{ 'A sorting algorithm is a list of instructions that arranges a set of values in a given order. I\'ve implemented three simple sorting alogrithms on this site, all of them arrange a randomly generated set of numbers from 100 to 600 in ascending order.' }</p><br></br>
+        <p>{ 'Simply a list of instructions that arranges a set of values in a given order. On this site I\'ve implemented three simple sorting methods that were invented before programming existed.' }</p><br></br>
         <p className="info-heading">What is Insertion sort?</p>
-        <p>{ ''}</p>
+        <p>{ 'Starting with the second element, compare all values its left, if value is smaller than the element, swap. It is typically used by people looking to sort their hand in a game of bridge. The algorithm considers each element one at a time, inserting each in its proper place among those already considered sorted.'}</p>
         <section className="button-container">
           <button
             className="close-button"

d910f489930fc5d460d15f1b564682c54d1e3ba4
Samuel Surgeon <ssurgeon00@icloud.com>
2020-02-29 15:03:48 +1100

    Finish text write up

--- src/components/InfoPopUp.js
+++ src/components/InfoPopUp.js
@@ -13,18 +13,18 @@ export default class InfoPopUp extends React.Component {
   render() {
     return (
       <section className="component-info-pop-up">
-        <p className="info-heading">What is a sorting algorithm?</p>
-        <p>{ 'Simply a list of instructions that arranges a set of values in a given order. On this site I\'ve implemented three simple sorting methods that were invented before programming existed.' }</p><br></br>
-        <p className="info-heading">What is Insertion sort?</p>
-        <p>{ 'Starting with the second element, compare all values its left, if value is smaller than the element, swap. It is typically used by people looking to sort their hand in a game of bridge. The algorithm considers each element one at a time, inserting each in its proper place among those already considered sorted.'}</p>
-        <section className="button-container">
-          <button
-            className="close-button"
-            onClick={this.handleClick}
-            alt="Close Pop-up Button">
-            ✕
-          </button>
-        </section>
+        <p className="info-heading-top">What is a sorting algorithm?</p>
+        <p>{ 'Simply a list of instructions that arranges a set of values in a given order. On this site I\'ve implemented three simple algorithms that sort in ascending order.' }</p>
+        <p className="info-heading">What is the performance of these algorithms?</p>
+        <p>Using Big-O notation each of these algorithms have an average time-complexity of O(n<sup>2</sup>).</p>
+        <p className="info-heading">Who are you?</p>
+        <p>My name is Samuel Surgeon. I{'\''}m a developer from Sydney. Say hi to me <a href="http://samuelsurgeon.com/" target="_blank">here</a>.</p>
+        <button
+          className="close-button"
+          onClick={this.handleClick}
+          alt="Close Pop-up Button">
+          ✕
+        </button>
       </section>
     );
   }

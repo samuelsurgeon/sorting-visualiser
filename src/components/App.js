66a77ea7dbc5f90ce7a2a3e09a98b1ea5803c031
Samuel Surgeon <ssurgeon00@icloud.com>
2020-03-12 13:23:48 +1100

    Add semantic titles to variables

--- src/components/App.js
+++ src/components/App.js
@@ -247,15 +247,6 @@ export default class App extends React.Component {
   }
 }
 
-// This can be used for testing...
-function arraysAreEqual(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) return false;
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) return false;
-  }
-  return true;
-}
-
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }

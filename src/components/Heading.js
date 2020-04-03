ab1d9c5a2ae8176b0d5b176bf8e15075d60220cc
Samuel Surgeon <ssurgeon00@icloud.com>
2020-04-03 14:37:30 +1100

    Remove constructor

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,10 +2,6 @@ import React from 'react';
 import './Heading.css';
 
 export default class Heading extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <section className="component-heading">

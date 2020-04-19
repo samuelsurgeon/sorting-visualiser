913fa6f9ba56b1570860afc5c2b1865c6520bd82
Samuel Surgeon <ssurgeon00@icloud.com>
2020-04-19 10:18:10 +1000

    Refactor code

--- src/logic/handleAction.js
+++ src/logic/handleAction.js
@@ -33,12 +33,14 @@ export function handleAction(unsortedArray, sortedArray, buttonName) {
     // Remember that I'm returning the active algorithm here. One other thing, I'm returning this three separate times, maybe there's a better way to handle it? Wait, do I even need to return activeAlgorithm? Let's try doing it without it.
    // return buttonName;
   }
+
   if (buttonName === 'bubble') {
     recolourAllElements('turquoise');
     selectTypeButton('bubble', 'turquoise');
 
     return getBubbleSortAnimations(unsortedArray);
   }
+
   if (buttonName === 'selection') {
     recolourAllElements('pink');
     selectTypeButton('selection', 'pink');
@@ -46,13 +48,65 @@ export function handleAction(unsortedArray, sortedArray, buttonName) {
     // These can be one-liners if you want it
     return getSelectionSortAnimations(unsortedArray);
   }
+
+  if (buttonName === 'sortButton') {
+    const speedSlider = document.querySelector(`input[class*='slider']`);
+    const animationSpeed = speedSlider.value;
+    const transitionSpeed = speedSlider.value / 1000;
+    const animations = sortAnimations;
+    // better implementation
+    let shouldColourSwap = true;
+
+    for (let i = 0; i < animations.length; i += 1) {
+      const arrayBars = document.querySelectorAll('.array-bar');
+      const barHeights = document.querySelectorAll('.bar-height');
+      const animateHighlight = typeof animations[i][0] === 'object' ? false : true;
+
+      if (animateHighlight) {
+        // maybe pack all this up into a function?
+        const [barOneIndex, barTwoIndex] = animations[i];
+        const barOneStyle = arrayBars[barOneIndex].style;
+        const barTwoStyle = arrayBars[barTwoIndex].style;
+        const colour = shouldColourSwap ? SECONDARY_COLOUR : PRIMARY_COLOUR;
+        setTimeout(() => {
+        barOneStyle.backgroundColor = colour;
+        barTwoStyle.backgroundColor = colour;
+        barOneStyle.transitionDuration = `${transitionSpeed}s`;
+        barTwoStyle.transitionDuration = `${transitionSpeed}s`;
+        }, i * animationSpeed);
+        shouldColourSwap = !shouldColourSwap;
+      } else {
+        setTimeout(() => {
+          const [barOneIndex, barOneHeight] = animations[i][0];
+          const [barTwoIndex, barTwoHeight] = animations[i][1];
+          const barOneStyle = arrayBars[barOneIndex].style;
+          const barTwoStyle = arrayBars[barTwoIndex].style;
+          barOneStyle.height = `${barOneHeight}px`;
+          barTwoStyle.height = `${barTwoHeight}px`;
+          const heightOneText = barHeights[barOneIndex];
+          const heightTwoText = barHeights[barTwoIndex];
+          heightOneText.textContent = `${barOneHeight}`;
+          heightTwoText.textContent = `${barTwoHeight}`;
+        }, i * animationSpeed);
+      }
+    }
+    setTimeout(() => {
+      const arrayBars = document.querySelectorAll('.array-bar');
+      let max = arrayBars.length;
+      for (let i = 0; i < max; i += 1) {
+        arrayBars[i].style.transitionDuration = `${transitionSpeed}s`;
+        arrayBars[i].style.backgroundColor = FINISHED_COLOUR;
+    }
+      const sortButtonStyle = document.querySelector(`button[class*='sort-button']`);
+      sortButtonStyle.textContent = 'Reset';
+    }, animations.length * animationSpeed);
+  }
 }
 
 export function generateArray() {
   const NUMBER_OF_ARRAY_BARS = 14;
   const ARRAY_BAR_MIN_HEIGHT = 100;
   const ARRAY_BAR_MAX_HEIGHT = 600;
-
   const arrayBars = document.querySelectorAll('.array-bar');
   const max = arrayBars.length;
   for (let i = 0; i < max; i += 1) {
@@ -69,37 +123,24 @@ function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function recolourAllElements(colour) {
+  document.body.className = colour;
+
+  const infoPopUpElement = document.querySelector(`section[class*='component-info-pop-up']`);
+  infoPopUpElement.className.includes('hidden') ? infoPopUpElement.className = `component-info-pop-up hidden ${colour}` : infoPopUpElement.className = `component-info-pop-up ${colour}`;
+
+  const sliderElement = document.querySelector(`input[class*='slider']`);
+  sliderElement.className = `slider ${colour}`;
+
+  const sortButtonElement = document.querySelector(`button[class*='sort-button']`);
+  sortButtonElement.className = `sort-button ${colour}`;
+}
+
 function selectTypeButton(algorithmType, colour) {
   const typeButton = document.querySelectorAll(`button[class*='type-button']`);
   typeButton.forEach(element => {
     element.getAttribute('name') === algorithmType ? element.className = `type-button ${colour} selected`: element.className = `type-button ${colour}`;
   });
-}
-
-function recolourAllElements(colour) {
-  recolourBody(colour);
-  recolourInfoPopUp(colour);
-  recolourSlider(colour);
-  recolourSortButton(colour);
-}
-
-function recolourBody(colour) {
-  document.body.className = colour;
-}
-
-function recolourInfoPopUp(colour) {
-  const infoPopUpElement = document.querySelector(`section[class*='component-info-pop-up']`);
-  infoPopUpElement.className.includes('hidden') ? infoPopUpElement.className = `component-info-pop-up hidden ${colour}` : infoPopUpElement.className = `component-info-pop-up ${colour}`;
-}
-
-function recolourSlider(colour) {
-  const sliderElement = document.querySelector(`input[class*='slider']`);
-  sliderElement.className = `slider ${colour}`;
-}
-
-function recolourSortButton(colour) {
-  const sortButtonElement = document.querySelector(`button[class*='sort-button']`);
-  sortButtonElement.className = `sort-button ${colour}`;
 }
 
 export function disableSortTypeButtons() {
@@ -123,57 +164,4 @@ export function clearAnimations(sortedArrayLength) {
   while (timeoutIDs--) {
     window.clearTimeout(timeoutIDs);
   }
-}
-
-export function setTimeoutsForAnimations(sortAnimations) {
-  const speedSlider = document.querySelector(`input[class*='slider']`);
-  const animationSpeed = speedSlider.value;
-  const transitionSpeed = speedSlider.value / 1000;
-  const animations = sortAnimations;
-  // better implementation
-  let shouldColourSwap = true;
-
-  for (let i = 0; i < animations.length; i += 1) {
-    const arrayBars = document.querySelectorAll('.array-bar');
-    const barHeights = document.querySelectorAll('.bar-height');
-    const animateHighlight = typeof animations[i][0] === 'object' ? false : true;
-
-    if (animateHighlight) {
-      // maybe pack all this up into a function?
-      const [barOneIndex, barTwoIndex] = animations[i];
-      const barOneStyle = arrayBars[barOneIndex].style;
-      const barTwoStyle = arrayBars[barTwoIndex].style;
-      const colour = shouldColourSwap ? SECONDARY_COLOUR : PRIMARY_COLOUR;
-      setTimeout(() => {
-      barOneStyle.backgroundColor = colour;
-      barTwoStyle.backgroundColor = colour;
-      barOneStyle.transitionDuration = `${transitionSpeed}s`;
-      barTwoStyle.transitionDuration = `${transitionSpeed}s`;
-      }, i * animationSpeed);
-      shouldColourSwap = !shouldColourSwap;
-    } else {
-      setTimeout(() => {
-        const [barOneIndex, barOneHeight] = animations[i][0];
-        const [barTwoIndex, barTwoHeight] = animations[i][1];
-        const barOneStyle = arrayBars[barOneIndex].style;
-        const barTwoStyle = arrayBars[barTwoIndex].style;
-        barOneStyle.height = `${barOneHeight}px`;
-        barTwoStyle.height = `${barTwoHeight}px`;
-        const heightOneText = barHeights[barOneIndex];
-        const heightTwoText = barHeights[barTwoIndex];
-        heightOneText.textContent = `${barOneHeight}`;
-        heightTwoText.textContent = `${barTwoHeight}`;
-      }, i * animationSpeed);
-    }
-  }
-  setTimeout(() => {
-    const arrayBars = document.querySelectorAll('.array-bar');
-    let max = arrayBars.length;
-    for (let i = 0; i < max; i += 1) {
-      arrayBars[i].style.transitionDuration = `${transitionSpeed}s`;
-      arrayBars[i].style.backgroundColor = FINISHED_COLOUR;
-  }
-    const sortButtonStyle = document.querySelector(`button[class*='sort-button']`);
-    sortButtonStyle.textContent = 'Reset';
-  }, animations.length * animationSpeed);
 }

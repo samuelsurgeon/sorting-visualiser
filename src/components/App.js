2f2b62eec07b1ae421bea248968747b690b44927
Samuel Surgeon <ssurgeon00@icloud.com>
2020-04-21 16:14:58 +1000

    Rename components

--- src/components/App.js
+++ src/components/App.js
@@ -1,65 +1,29 @@
 import React from 'react';
-import Heading from './Heading';
-import SortPanel from './SortPanel';
-import SpeedSlider from './SpeedSlider';
-import SortButton from './SortButton';
-import SortingVisualiser from './SortingVisualiser';
-import InfoButton from './InfoButton';
 import InfoPopUp from './InfoPopUp';
+import Heading from './Heading';
+import AlgorithmPanel from './AlgorithmPanel';
+import SpeedPanel from './SpeedPanel';
+import SortButton from './SortButton';
+import InfoButton from './InfoButton';
+import SortingVisualiser from './SortingVisualiser';
 import './App.css';
 
-import { generateArray } from '../logic/generateArray';
-import { handleAction } from '../logic/handleAction';
-
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // do I really need all of these?
-      activeAlgorithm: null,
-      unsortedArray: null,
-      // get rid of sortedArray, just use an array slot and update it
-      sortedArray: null,
-      // rename this to something more semantic, such as animationsRunning or something
-      sortButtonClicked: false,
-    };
-  }
-
-  componentDidMount() {
-    this.setState({ unsortedArray: generateArray() });
-  }
-  
-  shouldComponentUpdate() {
-    if (this.state.sortButtonClicked) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  handleClick = buttonName => {
-    let boy = handleAction(this.state, buttonName);
-    this.setState({ boy });
-  }
-
   render() {
     return (
       <section className="component-app">
-        <InfoPopUp clickHandler={this.handleClick}></InfoPopUp>
+        <InfoPopUp />
         <section className="sidebar">
-          <Heading></Heading>
+          <Heading />
           <section className="sidebar-bottom">
-            <SortPanel clickHandler={this.handleClick}></SortPanel>
-            <SpeedSlider></SpeedSlider>
-            <SortButton clickHandler={this.handleClick}></SortButton>
+            <AlgorithmPanel />
+            <SpeedPanel />
+            <SortButton />
           </section>
         </section>
         <section className="visualiser">
-          <InfoButton clickHandler={this.handleClick}></InfoButton>
-          <SortingVisualiser 
-            unsortedArray={this.state.unsortedArray}
-            sortedArray={this.state.sortedArray}
-          ></SortingVisualiser>
+          <InfoButton />
+          <SortingVisualiser />
         </section>
       </section>
     );

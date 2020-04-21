7cce17a3112bb1a798b2d0bb712e1c2a416132b6
Samuel Surgeon <ssurgeon00@icloud.com>
2020-04-21 17:14:59 +1000

    Remove functionality

--- src/components/SortButton.js
+++ src/components/SortButton.js
@@ -2,17 +2,12 @@ import React from 'react';
 import './SortButton.css';
 
 export default class SortButton extends React.Component {
-  handleClick = clickedButton => {
-    this.props.clickHandler(clickedButton.target.name);
-  }
-
   render() {
     return (
       <section className="component-sort-button">
         <button 
           name="sortButton"
-          className="sort-button"
-          onClick={this.handleClick}>
+          className="sort-button">
           Sort
         </button>
       </section>

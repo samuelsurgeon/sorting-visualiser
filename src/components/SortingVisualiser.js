344d819c53dbfffd48529185d9dc5d048883ab2e
Samuel Surgeon <ssurgeon00@icloud.com>
2020-04-23 09:33:02 +1000

    Remove interactivity

--- src/components/SortingVisualiser.js
+++ src/components/SortingVisualiser.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import './SortingVisualiser.css';
 
-// I think the way to solve the problem of importing multiple functions from the same .js (bad) is to create a separate .js file for each function, there was another repo that did that. I think it was C
-import { getInsertionSortAnimations } from '../logic/algorithms/insertion';
-import { getBubbleSortAnimations } from '../logic/algorithms/bubble';
-import { getSelectionSortAnimations } from '../logic/algorithms/selection';
-
 export default class SortingVisualiser extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-
   render() {
-    // think of a better more obvious way to do this. face the issue head, deal with the asyncness in App.js
-    const unsortedArray = this.props.unsortedArray ? this.props.unsortedArray : [];
+    //const unsortedArray = this.props.unsortedArray ? this.props.unsortedArray : [];
+    const array = [];
     return (
       <section className="component-sorting-visualiser">
-        {unsortedArray.map((height, index) => (
+        {array.map((height, index) => (
           <section
             className="array-bar"
             key={index}

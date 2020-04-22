20f99db3ab59147125a5ebb995dce19aad7cd181
Samuel Surgeon <ssurgeon00@icloud.com>
2020-04-23 09:29:40 +1000

    Remove interactivity

--- src/components/InfoButton.js
+++ src/components/InfoButton.js
@@ -2,14 +2,6 @@ import React from 'react';
 import './InfoButton.css';
 
 export default class InfoButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  handleClick = clickedButton => {
-    this.props.clickHandler(clickedButton.target.name);
-  }
-
   render() {
     return (
       <section className="component-info-button">
@@ -17,7 +9,6 @@ export default class InfoButton extends React.Component {
           <button
             name="infoButton"
             className="info-button"
-            onClick={this.handleClick}
             alt="Information Button">
             ?
           </button>

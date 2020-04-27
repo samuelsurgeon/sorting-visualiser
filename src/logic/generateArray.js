a68d46a6bd42bb66d83c5dedcc5429dccec10b7a
Samuel Surgeon <ssurgeon00@icloud.com>
2020-04-27 23:10:03 +1000

    Generate arrays based on users screen size

--- src/logic/generateArray.js
+++ src/logic/generateArray.js
@@ -1,8 +1,8 @@
 export default function generateArray() {
   const PRIMARY_COLOUR = '#00000033';
   const NUMBER_OF_ARRAY_BARS = 14;
-  const ARRAY_BAR_MIN_HEIGHT = 100;
-  const ARRAY_BAR_MAX_HEIGHT = 600;
+  const ARRAY_BAR_MIN_HEIGHT = window.screen.height / 12;
+  const ARRAY_BAR_MAX_HEIGHT = window.screen.height / 2;
   const arrayBars = document.querySelectorAll('.array-bar');
   const max = arrayBars.length;
   for (let i = 0; i < max; i += 1) {
